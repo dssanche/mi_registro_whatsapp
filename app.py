73f2a248fbadb570d9b3cee1fc5601a13f6ff77c
dssanche <ds.sanchez@uniandes.edu.co>
2025-10-12 18:53:56 -0500

    Fix app.py

--- app.py
+++ app.py
@@ -73,4 +73,14 @@ def registro():
     return redirect(WHATSAPP_GROUP_LINK)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if 'RENDER' in os.environ:
+        # En producción
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)
+    else:
+        # En desarrollo local
+        app.run(debug=True, host='0.0.0.0', port=5000)
+        
+
+##if __name__ == '__main__':
+  ##  app.run(debug=True, host='0.0.0.0', port=5000)
